45915e35bbea1d968ccc741a99b25c1dc71e46a9
nancylko <nancy18lko.singh@gmail.com>
2021-04-13 02:39:21 -0700

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
-import {MatGridListModule} from '@angular/material/grid-list';
-import { MatCardModule} from '@angular/material/card'; 
-import {MatDialogModule} from '@angular/material/dialog'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppComponent } from './app.component';
 import { CommonServiceService } from './common-service.service';
@@ -30,12 +26,8 @@ import { SpaceprogPanelComponent } from './spaceprog-panel/spaceprog-panel.compo
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    MatGridListModule,
-    MatCardModule,
-    MatDialogModule,
     BrowserAnimationsModule,
-    Ng2SearchPipeModule,
-     NgxSpinnerModule
+    NgxSpinnerModule
   ],
   providers: [CommonServiceService],
   bootstrap: [AppComponent],
